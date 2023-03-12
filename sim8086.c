e5ee44a4854dde1f9c558d1f8e08b86c32b71fa3
Ernes Budiman <ernes@alegrium.com>
2023-03-12 12:45:02 +0700

    finished listing 40

--- sim8086.c
+++ sim8086.c
@@ -20,13 +20,19 @@
   (val & 0x02 ? '1' : '0'), \
   (val & 0x01 ? '1' : '0') 
 
-char* regTable[16] = 
+char* regTable8[8] = 
 {
     "al", "cl", "dl", "bl", "ah", "ch", "dh", "bh",
+};
+
+char* regTable16[8] = 
+{
     "ax", "cx", "dx", "bx", "sp", "bp", "si", "di"
 };
 
-char* rmTable[8] = 
+char** regTable[2] = { regTable8, regTable16 };
+
+char* romTable[8] = 
 {
     "bx + si", "bx + di", "bp + si", "bp + di", "si", "di", "bp", "bx"
 };
@@ -57,7 +63,7 @@ int main(int argc, char **argv)
     u8* buffer = malloc(fileSize);
     int count = fread(buffer, sizeof(char), fileSize, file);
     fclose(file);
-
+        
     printf("bits 16\n\n");
     u32 offset = 0;
     while(offset < fileSize)
@@ -65,38 +71,47 @@ int main(int argc, char **argv)
         u8 byte1 = buffer[offset++];
         u8 byte2 = buffer[offset++];
 
-        u8 opCode = (byte1 >> 4);
-
         //printf("byte1 %c%c%c%c%c%c%c%c\n", BYTE_TO_BINARY(byte1));
         //printf("byte2 %c%c%c%c%c%c%c%c\n", BYTE_TO_BINARY(byte2));
-        
-        if(opCode == 0b1000) // Register/memory to/from register\n
+
+        // Note: https://edge.edx.org/c4x/BITSPilani/EEE231/asset/8086_family_Users_Manual_1_.pdf
+        // Intel manual 8086 guide -- Machine Instruction Decoding Guide
+        switch(byte1)
         {
-            u8 d = (byte1 >> 1) & 0b1;
-            u8 w = (byte1 >> 0) & 0b1;
+        case 0x85:
+        {
+        } break;
+
+        case 0x88:
+        case 0x89:
+        case 0x8a:
+        case 0x8b:
+        case 0x8c:
+        {
+            u8 dir = (byte1 >> 1) & 0b1;
+            u8 wide = (byte1 >> 0) & 0b1;
             u8 mod = (byte2 >> 6) & 0b11;
             u8 reg = (byte2 >> 3) & 0b111;
-            u8 rm = (byte2 >> 0) & 0b111;
-
-            if(w)
-            {
-                reg |= 0b1000;
-            }
+            u8 rom = (byte2 >> 0) & 0b111;
 
             if(mod == 0b00)
             {
-                if(rm == 0b110)
+                if(rom == 0b110)
                 {
+                    u8 byte3 = buffer[offset++];
+                    u8 byte4 = buffer[offset++];
+                    s16 data = (byte4 << 8) | byte3;
+                    printf("mov %s, [%d]\n", regTable[wide][reg], data);
                 }
                 else
                 {   
-                    if(d)
+                    if(dir)
                     {
-                        printf("mov %s, [%s]\n", regTable[reg], rmTable[rm]);
+                        printf("mov %s, [%s]\n", regTable[wide][reg], romTable[rom]);
                     }
                     else
                     {
-                        printf("mov [%s], %s\n", rmTable[rm], regTable[reg]);
+                        printf("mov [%s], %s\n", romTable[rom], regTable[wide][reg]);
                     }
                 }
             }
@@ -104,72 +119,148 @@ int main(int argc, char **argv)
             {
                 s8 data = buffer[offset++];
 
-                if(d)
+                if(dir)
                 {
-                    printf("mov %s, [%s + %d]\n", regTable[reg], rmTable[rm], data);
+                    printf("mov %s, [%s + %d]\n", regTable[wide][reg], romTable[rom], data);
                 }
                 else
                 {
-                    printf("mov [%s + %d], %s\n", rmTable[rm], data, regTable[reg]);
+                    printf("mov [%s + %d], %s\n", romTable[rom], data, regTable[wide][reg]);
                 }
             }
             else if(mod == 0b10)
             {
                 u8 byte3 = buffer[offset++];
                 u8 byte4 = buffer[offset++];
-                s16 data = byte3 | (byte4 << 8);
+                s16 data = (byte4 << 8) | byte3;
 
-                if(d)
+                if(dir)
                 {
-                    printf("mov %s, [%s + %d]\n", regTable[reg], rmTable[rm], data);
+                    printf("mov %s, [%s + %d]\n", regTable[wide][reg], romTable[rom], data);
                 }
                 else
                 {
-                    printf("mov [%s + %d], %s\n", rmTable[rm], data, regTable[reg]);
+                    printf("mov [%s + %d], %s\n", romTable[rom], data, regTable[wide][reg]);
                 }
             }
             else if(mod == 0b11)
             {
-                if(w)
-                {
-                    rm |= 0b1000;
-                }
+                u8 src = dir ? reg : rom;
+                u8 dest = dir ? rom : reg;
 
-                u8 src = d ? reg : rm;
-                u8 dest = d ? rm : reg;
-
-                printf("mov %s, %s\n", regTable[src], regTable[dest]);
+                printf("mov %s, %s\n", regTable[wide][src], regTable[wide][dest]);
             }
-        }
-        else if(opCode == 0b1100)
-        {
-            printf("Immediate register to memory\n");
-        }
-        else if(opCode == 0b1011) // Immediate to register 
-        {
-            u8 w = (byte1 >> 3) & 0b1;
-            u8 reg = (byte1 >> 0) & 0b111;
+        } break;
 
-            if(w)
+        case 0xa0:
+        case 0xa1:
+        case 0xa2:
+        case 0xa3:
+        {
+            u8 dir = (byte1 >> 1) & 0b1;
+            u8 wide = (byte1 >> 0) & 0b1;
+
+            u8 byte3 = buffer[offset++];
+            s16 data = (byte3 << 8) | byte2;
+
+            if(dir)
             {
-                reg |= 0b1000;
-                u8 byte3 = buffer[offset++];
-                s16 data = byte2 | (byte3 << 8);
-
-                printf("mov %s, %d\n", regTable[reg], data);
+                printf("mov [%d], %s\n", data, regTable[wide][0]);
             }
             else
             {
-                printf("mov %s, %d\n", regTable[reg], byte2);
+                printf("mov %s, [%d]\n", regTable[wide][0], data);
+            }
+        } break;
+
+        case 0xb0:
+        case 0xb1:
+        case 0xb2:
+        case 0xb3:
+        case 0xb4:
+        case 0xb5:
+        case 0xb6:
+        case 0xb7:
+        case 0xb8:
+        case 0xb9:
+        case 0xba:
+        case 0xbb:
+        case 0xbc:
+        case 0xbd:
+        case 0xbe:
+        case 0xbf:
+        {
+            u8 wide = (byte1 >> 3) & 0b1;
+            u8 reg = (byte1 >> 0) & 0b111;
+
+            if(wide)
+            {
+                u8 byte3 = buffer[offset++];
+                s16 data = (byte3 << 8) | byte2;
+
+                printf("mov %s, %d\n", regTable[wide][reg], data);
+            }
+            else
+            {
+                printf("mov %s, %d\n", regTable[wide][reg], byte2);
+            }
+        } break;
+
+        case 0xc6:
+        case 0xc7:
+        {
+            u8 wide = (byte1 >> 0) & 0b1;
+            u8 mod = (byte2 >> 6) & 0b11;
+            u8 rom = (byte2 >> 0) & 0b111;
+
+            char* size = wide ? "word" : "byte";
+
+            if(mod == 0b00)
+            {
+                if(rom == 0b110)
+                {
+                }
+                else
+                {   
+                    if(wide)
+                    {
+                        u8 byte3 = buffer[offset++];
+                        u8 byte4 = buffer[offset++];
+                        s16 data = (byte4 << 8) | byte3;
+                        printf("mov [%s], word %d\n", romTable[rom], data);
+                    }
+                    else
+                    {
+                        s8 data = buffer[offset++];
+                        printf("mov [%s], byte %d\n", romTable[rom], data);
+                    }
+                }
+            }
+            else if(mod == 0b01)
+            {
+                s8 displacement = buffer[offset++];
+                s8 data = buffer[offset++];
+                printf("mov [%s + %d], %s %d\n", romTable[rom], displacement, size, data);
+            }
+            else if(mod == 0b10)
+            {
+                u8 byte3 = buffer[offset++];
+                u8 byte4 = buffer[offset++];
+                u8 byte5 = buffer[offset++];
+                u8 byte6 = buffer[offset++];
+                s16 displacement = (byte4 << 8) | byte3;
+                s16 data = (byte6 << 8) | byte5;
+
+                printf("mov [%s + %d], %s %d\n", romTable[rom], displacement, size, data);
             }
         }
-        else if(opCode == 0b1010)
+        break;
+
+        default: 
         {
-            printf("Memory to accumulator | accumulator to memory \n");
-        }
-        else if(opCode == 0b1010)
-        {
-            printf("Register/memory to segment register | Segment register to register/memory");
+            printf("0x%x unimplemented\n", byte1);
+        } break;
+
         }
     }
 
--- test.sh
+++ test.sh
@@ -8,22 +8,30 @@ nasm listing37.asm -o test/listing37
 nasm listing38.asm -o test/listing38
 nasm listing39.asm -o test/listing39
 nasm listing40.asm -o test/listing40
+#nasm listing41.asm -o test/listing41
+#nasm listing42.asm -o test/listing42
 
 pushd test > /dev/null
 ../sim8086 listing37 >> listing37Test.asm
 ../sim8086 listing38 >> listing38Test.asm
 ../sim8086 listing39 >> listing39Test.asm
 ../sim8086 listing40 >> listing40Test.asm
+#../sim8086 listing41 >> listing41Test.asm
+#../sim8086 listing42 >> listing42Test.asm
 
 nasm listing37Test.asm -o listing37Test
 nasm listing38Test.asm -o listing38Test
 nasm listing39Test.asm -o listing39Test
 nasm listing40Test.asm -o listing40Test
+#nasm listing41Test.asm -o listing41Test
+#nasm listing42Test.asm -o listing42Test
 
 diff -s listing37 listing37Test
 diff -s listing38 listing38Test
 diff -s listing39 listing39Test
 diff -s listing40 listing40Test
+#diff -s listing41 listing41Test
+#diff -s listing42 listing42Test
 
 popd > /dev/null
 rm -r test
