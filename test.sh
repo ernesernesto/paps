23694ab143673e1984f4e9328d6e558492fd36f3
Ernes Budiman <ernes@alegrium.com>
2023-04-01 16:13:40 +0700

    add as gitmodules and cleanup

--- test.sh
+++ test.sh
@@ -1,39 +1,50 @@
 #!/usr/bin/env bash
 
-mkdir test
-
 clang -o sim8086 sim8086.c 
 
-nasm listing37.asm -o test/listing37
-nasm listing38.asm -o test/listing38
-nasm listing39.asm -o test/listing39
-nasm listing40.asm -o test/listing40
-nasm listing41.asm -o test/listing41
-#nasm listing42.asm -o test/listing42
+sim="../../../../sim8086"
 
-pushd test > /dev/null
-../sim8086 listing37 >> listing37Test.asm
-../sim8086 listing38 >> listing38Test.asm
-../sim8086 listing39 >> listing39Test.asm
-../sim8086 listing40 >> listing40Test.asm
-../sim8086 listing41 >> listing41Test.asm
-#../sim8086 listing42 >> listing42Test.asm
+dir="computer_enhance/perfaware/part1/test"
+mkdir $dir
+pushd $dir > /dev/null
 
-nasm listing37Test.asm -o listing37Test
-nasm listing38Test.asm -o listing38Test
-nasm listing39Test.asm -o listing39Test
-nasm listing40Test.asm -o listing40Test
-nasm listing41Test.asm -o listing41Test
-#nasm listing42Test.asm -o listing42Test
+nasm ../listing_0037_single_register_mov.asm -o listing_0037_single_register_mov
+nasm ../listing_0038_many_register_mov.asm -o listing_0038_many_register_mov
+nasm ../listing_0039_more_movs.asm -o listing_0039_more_movs
+nasm ../listing_0040_challenge_movs.asm -o listing_0040_challenge_movs
+nasm ../listing_0041_add_sub_cmp_jnz.asm -o listing_0041_add_sub_cmp_jnz
 
-diff -s listing37 listing37Test
-diff -s listing38 listing38Test
-diff -s listing39 listing39Test
-diff -s listing40 listing40Test
-diff -s listing41 listing41Test
-#diff -s listing42 listing42Test
+
+$sim listing_0037_single_register_mov >> listing_0037_single_register_mov_test.asm
+$sim listing_0038_many_register_mov >> listing_0038_many_register_mov_test.asm
+$sim listing_0039_more_movs >> listing_0039_more_movs_test.asm
+$sim listing_0040_challenge_movs >> listing_0040_challenge_movs_test.asm
+$sim listing_0041_add_sub_cmp_jnz >> listing_0041_add_sub_cmp_jnz_test.asm
+
+nasm listing_0037_single_register_mov_test.asm -o listing_0037_single_register_mov_test
+nasm listing_0038_many_register_mov_test.asm -o listing_0038_many_register_mov_test
+nasm listing_0039_more_movs_test.asm -o listing_0039_more_movs_test
+nasm listing_0040_challenge_movs_test.asm -o listing_0040_challenge_movs_test
+nasm listing_0041_add_sub_cmp_jnz_test.asm -o listing_0041_add_sub_cmp_jnz_test
+
+diff -w -s listing_0037_single_register_mov listing_0037_single_register_mov_test
+diff -w -s listing_0038_many_register_mov listing_0038_many_register_mov_test
+diff -w -s listing_0039_more_movs listing_0039_more_movs_test
+diff -w -s listing_0040_challenge_movs listing_0040_challenge_movs_test
+diff -w -s listing_0041_add_sub_cmp_jnz listing_0041_add_sub_cmp_jnz_test
+
+#####
+
+nasm ../listing_0043_immediate_movs.asm -o listing_0043_immediate_movs
+nasm ../listing_0044_register_movs.asm -o listing_0044_register_movs
+
+$sim -exec listing_0043_immediate_movs >> listing_0043_immediate_movs_test.txt 
+$sim -exec listing_0044_register_movs >> listing_0044_register_movs_test.txt 
+
+diff -w -s ../listing_0043_immediate_movs.txt listing_0043_immediate_movs_test.txt 
+diff -w -s ../listing_0044_register_movs.txt listing_0044_register_movs_test.txt 
 
 popd > /dev/null
-rm -r test
+rm -r $dir
 
 echo "Test Complete!"
